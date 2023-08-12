614ba5ceed35aa86342a776f0a8c79ba6cd43d44
Francis Yang <fy50167@gmail.com>
2023-08-12 04:42:27 -0700

    First iteration of markdown generation function

--- Develop/index.js
+++ Develop/index.js
@@ -5,15 +5,62 @@ const questions = ['What is your project title?',
 'Enter a description for your project.', 
 "What are the application's installation instructions?", 
 "How is the application used?", 
+"Who else contributed to this project?",
 "What are the guidelines for contributing?", 
 "What are the test instructions?", 
 "What license are you using?", 
 "What is your GitHub username?", 
 "What is your email?"];
-const [title, description, installation, usage, contributions, testing, license, username, email] = questions; // Array deconstruction
+const [title, description, installation, usage, contributors, contributions, testing, license, username, email] = questions; // Array deconstruction
+
+const generateMarkdown = ({projectTitle, projectDescription, projectInstallation, projectCredits, projectUsage, projectContributions, projectTesting, projectLicense, projectUsername, projectEmail}) =>
+    `# ${projectTitle} 
+
+    ## Description
+    
+    ${projectDescription}
+    
+    ## Table of Contents
+    
+    If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+    - [Installation]
+    - [Usage]
+    - [Credits]
+    - [License]
+    
+    ## Installation
+    
+    ${projectInstallation}
+    
+    ## Usage
+    
+    ${projectUsage}
+    
+    ## Credits
+    
+    ${projectCredits}
+    
+    ## License
+    
+    ${projectLicense}
+    
+    ## How to Contribute
+    
+    ${projectContributions}
+
+    ## Tests
+    
+   ${projectTesting}
+
+   ## Contact Information
+
+   ${projectUsername}
+   ${projectEmail}
+   `;
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+    fs.writeFile(fileName, JSON.stringify(data), (err) =>
     err ? console.error(err) : console.log('File generated!')
     )
 }
@@ -23,51 +70,62 @@ function init() {
   .prompt([
     {
         type: "input",
-        name: "title",
+        name: "projectTitle",
         message: title
     },
     {
         type: "input",
-        name: "description",
+        name: "projectDescription",
         message: description
     },
     {
         type: "input",
-        name: "installation",
+        name: "projectInstallation",
         message: installation
     },
     {
         type: "input",
-        name: "usage",
+        name: "projectUsage",
         message: usage
     },
     {
         type: "input",
-        name: "contributions",
+        name: "projectContributors",
+        message: contributors
+    },
+    {
+        type: "input",
+        name: "projectContributions",
         message: contributions
     },
     {
         type: "input",
-        name: "testing",
+        name: "projectTesting",
         message: testing
     },
     {
         type: "list",
-        name: "license",
+        name: "projectLicense",
         message: license,
-        choices: []
+        choices: ['License 1', 'License 2']
     },
     {
         type: "input",
-        name: "username",
+        name: "projectUsername",
         message: username
     },
     {
-        tyoe: "inout",
-        name: "email",
+        tyoe: "input",
+        name: "projectEmail",
         message: email
     }
     ])
+    .then((answers) => {
+        console.log(answers);
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+    } 
+    )
 }
 
 // Function call to initialize app
